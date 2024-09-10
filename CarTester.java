66a0ee7d9dbb9cf2347af46487e786ba3d97d5a0
aleu7819 <allison.asl.leung@gmail.com>
2024-09-10 15:28:27 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,14 @@
-
+public class CarTester {
+    
+    public Car addCar(){
+        //implment
+    }
+    
+    public static void main(String[] args) {
+        //call addcar at lest twice and print car
+    }
+    
+    
+    
+    
+}
