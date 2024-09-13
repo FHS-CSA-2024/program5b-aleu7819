5722b21e9b9f483ebe4a319e118d47f300ec2f15
aleu7819 <allison.asl.leung@gmail.com>
2024-09-12 20:55:25 -0700

    Update Car.java
    finished

--- Car.java
+++ Car.java
@@ -1,37 +1,59 @@
-public car (String carName, int milesDriven, int gallonsUsed) {
-}
-        
-        
+
+import java.util.Scanner;
+
+class Car { 
+
+       private String carName; 
+       private double milesDriven;
+       private double gallonsUsed;
+    
+       public Car() {
+           carName = "";
+           milesDriven = 0;
+           gallonsUsed =0;
+           // construct with no param
+       }
+       
+       public Car (String newName, double newGallons, double newMiles){
+           
+           carName = newName;
+           milesDriven = newMiles;
+           gallonsUsed = newGallons;
+           // construct with param
+           
+       }
+       //getters and setters for all param
+       
         public String getName(){
-            
+            return carName;
+        }
+        public void setName(String newName) {
+            carName = newName;
+        }
+        
+        public double getMiles(){
+            return milesDriven;
+        }
+        public void setMiles(double newMiles) {
+            milesDriven = newMiles;
+        }
+        
+        public double getGallons(){
+            return gallonsUsed;
+        }
+        public void setGallons(double newGallons) {
+            gallonsUsed = newGallons;
         }
         
         
-        public void setName(String newname) {
-            
-        }
-        
-        public double calculateaverage(){
-            
+        public double calculateAverage(){
+            return (milesDriven/gallonsUsed * 10) / 10.0;
         }
         
         public String toString(){
-            
+            return carName + " averaged " + calculateAverage() + " m/g ";
         }
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
-}
+
